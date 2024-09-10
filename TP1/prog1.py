e67330c8d4f5a59be5624311a83c121471b60c3e
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-10 11:03:01 +0200

    modif fichier 2

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,11 +1,16 @@
 print ( " Hello , World ! " )
 
-# Demander à l'utilisateur de saisir un nombre
-nombre = float(input("Entrez un nombre : "))
+while True:
+    try:
+        # Demander à l'utilisateur de saisir un nombre
+        nombre = float(input("Entrez un nombre (ou appuyez sur CTRL-C pour quitter) : "))
 
-# Calculer le carré du nombre
-carre = nombre ** 2
+        # Calculer le carré du nombre
+        carre = nombre ** 2
 
-# Afficher le carré du nombre
-print(f"Le carré de {nombre} est {carre}.")
+        # Afficher le carré du nombre
+        print(f"Le carré de {nombre} est {carre}.")
+        
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
 
