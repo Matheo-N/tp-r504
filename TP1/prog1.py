d8d6cc11493c33d5c2ec4330c96f34f4d1e28150
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-10 11:00:25 +0200

    modif fichier

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,11 @@
 print ( " Hello , World ! " )
+
+# Demander à l'utilisateur de saisir un nombre
+nombre = float(input("Entrez un nombre : "))
+
+# Calculer le carré du nombre
+carre = nombre ** 2
+
+# Afficher le carré du nombre
+print(f"Le carré de {nombre} est {carre}.")
+
