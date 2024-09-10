4e17cd439ef12306718dc4961f99aa83f72b9d3a
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-10 11:26:10 +0200

    changement Q2.6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,1 +1,12 @@
-t
+def puissance(a, b):
+    """
+    Calcule a élevé à la puissance b.
+    Vérifie que a et b sont des entiers.
+    
+    Lève une exception TypeError si ce n'est pas le cas.
+    """
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError("Les arguments doivent être des entiers.")
+    
+    return a ** b
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,16 +1,25 @@
-print ( " Hello , World ! " )
+import fonctions as f
 
 while True:
     try:
-        # Demander à l'utilisateur de saisir un nombre
-        nombre = float(input("Entrez un nombre (ou appuyez sur CTRL-C pour quitter) : "))
+        # Demander à l'utilisateur de saisir les deux nombres
+        a = float(input("Nombre entier : "))
+        b = float(input("Exposant entier : "))
 
-        # Calculer le carré du nombre
-        carre = nombre ** 2
+        # Vérifier si les valeurs sont des entiers
+        if not a.is_integer() or not b.is_integer():
+            raise ValueError("Valeurs entières nécéssaire")
 
-        # Afficher le carré du nombre
-        print(f"Le carré de {nombre} est {carre}.")
-        
-    except ValueError:
-        print("Veuillez entrer un nombre valide.")
+        # Convertir les valeurs en entiers
+        a = int(a)
+        b = int(b)
+
+        # Calculer la puissance en utilisant la fonction
+        resultat = f.puissance(a, b)
+
+        # Afficher le résultat
+        print(f"{a} puissance {b} égal à {resultat}.")
+    
+    except (ValueError, TypeError) as e:
+        print(f"Erreur : {e}")
 
