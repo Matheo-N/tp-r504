7fce4e91433202e6d179f41129954e468159b8d3
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-23 09:16:30 +0200

    ajout des fichiers

---
+++ TP4/data.sql
@@ -0,0 +1,8 @@
+CREATE DATABASEdemosql;
+USE demosql;
+CREATE TABLE myTable(id int AUT0._INCREMENT, name varchar(45) N0T NULL
+	, PRIMARY KEY (id) );
+INSERT INT0 myTable (id name. VALUES (NULL, 'bob');
+INSERT INT0 myTable (id, name. VALUES (NULL,  'alice');
+INSERT INT0 myTable (id name. VALUES (NULL,  'john');
+
