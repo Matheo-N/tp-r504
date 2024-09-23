9bc260cdb30d64e689fb096e96424128b3753fff
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-23 11:16:30 +0200

    changement fin de scéance du 23/09

---
+++ TP4/run_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Nom du conteneur
+CONTAINER_NAME=tp4-app
+
+# Nom de l'image
+IMAGE_NAME=im-tp4
+
+# Réseau
+NETWORK_NAME=net-tp4
+
+# Port à publier
+PORT=5000
+
+# Fonction pour arrêter le conteneur si il existe déjà
+function stop_container() {
+  docker stop $CONTAINER_NAME &> /dev/null
+  docker rm $CONTAINER_NAME &> /dev/null
+}
+
+# Arrête le conteneur si il existe déjà
+stop_container
+
+# Lance le conteneur
+docker run \
+  --name $CONTAINER_NAME \
+  --network $NETWORK_NAME \
+  -p $PORT:$PORT \
+  $IMAGE_NAME
