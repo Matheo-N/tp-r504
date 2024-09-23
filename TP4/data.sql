5a431b3c55032e7f854796c7682bdbca7e453cf4
Matheo-N <matheo.nouvel@univ-rouen.fr>
2024-09-23 09:50:06 +0200

    sql change

--- TP4/data.sql
+++ TP4/data.sql
@@ -2,7 +2,7 @@ CREATE DATABASE demosql;
 USE demosql;
 CREATE TABLE myTable(id int AUT0._INCREMENT, name varchar(45) N0T NULL
 	, PRIMARY KEY (id) );
-INSERT INT0 myTable (id name. VALUES (NULL, 'bob');
-INSERT INT0 myTable (id, name. VALUES (NULL,  'alice');
-INSERT INT0 myTable (id name. VALUES (NULL,  'john');
+INSERT INTO myTable (id name) VALUES (NULL, 'bob');
+INSERT INTO myTable (id, name) VALUES (NULL,  'alice');
+INSERT INTO myTable (id name) VALUES (NULL,  'john');
 
